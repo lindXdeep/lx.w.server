e1cc8e1f3c9da5d929c3f193d302c1a0405daff3
lindx <linder.igor@gmail.com>
2022-07-31 08:42:08 +0300

    WebAppContext

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,31 @@
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * App
  */
 public class App {
 
+  public static String webdir = "src/main/webapp";
+  public static String descriptor = webdir + "/WEB-INF/web.xml";
+
   public static void main(String[] args) throws Exception {
-    
+
     Server server = new Server(8080);
 
-    ContextHandler ch = new ContextHandler();
-    ch.setContextPath("/hello");
-    ch.setHandler(new MyHandler()); // A `ContextHandler` is instantiated and a path is set.
-    
-    server.setHandler(ch);          // The handler is set for the context handler object.
-    
+    WebAppContext wc = new WebAppContext();
+    wc.setContextPath("/myapp");
+    wc.setDescriptor(descriptor); // deployment descriptor
+    wc.setResourceBase(webdir);   // document root for the static resources for the context. 
+    wc.setParentLoaderPriority(true);
+
+    server.setHandler(wc);        // The handler is set for the context handler object.
+
+    server.dumpStdErr();
+
+
     server.start();
     server.join();
-    
+
   }
 }
