a566f3224c11bd9d73692eec76354faa8f0ab565
lindx <linder.igor@gmail.com>
2022-08-04 07:02:52 +0300

    ContextHandler

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,23 @@
 import org.eclipse.jetty.server.Server;
+<<<<<<< HEAD
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
+=======
+import org.eclipse.jetty.webapp.WebAppContext;
+>>>>>>> parent of ed8d4e8 (ServletHandler)
 
+/**
+ * App
+ */
 public class App {
+
+  public static String webdir = "src/main/webapp";
+  public static String descriptor = webdir + "/WEB-INF/web.xml";
+
   public static void main(String[] args) throws Exception {
 
+<<<<<<< HEAD
     ContextHandler ch = new ContextHandler(); // используется только для ответа на запросы соответствующие URI
     ResourceHandler rh = new ResourceHandler();
 
@@ -20,6 +32,20 @@ public class App {
     ch.setHandler(rh);
 
     server.setHandler(ch); // регистрируем на сервере ContextHandler
+=======
+    Server server = new Server(8080);
+
+    WebAppContext wc = new WebAppContext();
+    wc.setContextPath("/myapp");
+    wc.setDescriptor(descriptor); // deployment descriptor
+    wc.setResourceBase(webdir);   // document root for the static resources for the context. 
+    wc.setParentLoaderPriority(true);
+
+    server.setHandler(wc);        // The handler is set for the context handler object.
+
+    server.dumpStdErr();
+
+>>>>>>> parent of ed8d4e8 (ServletHandler)
 
     server.start();
     server.join();
