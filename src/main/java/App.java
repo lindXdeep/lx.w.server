d532ec3328f113be0587bbe780a7b4e1cc05f122
lindx <linder.igor@gmail.com>
2022-08-08 13:15:05 +0300

    ContextHandler

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 
 /**
@@ -8,11 +10,18 @@ public class App {
 
   public static void main(String[] args) throws Exception {
 
-    ServletHandler sh = new ServletHandler();
-    sh.addServletWithMapping(MyServlet.class, "/");
+    ResourceHandler rh = new ResourceHandler();
+    rh.setDirectoriesListed(true);
+    rh.setWelcomeFiles(new String[] { "index.jsp" }); // файл кторый должен быть предоставлен запрашивающей стороне
+    rh.setResourceBase("src/main/webapp");
+
+    ContextHandler ch = new ContextHandler(); // используется только для ответа на запросы соответствующие URI
+    ch.setContextPath("/hello");
+    ch.setHandler(rh);
 
     Server server = new Server(8080);
-    server.setHandler(sh);
+    server.setHandler(ch);
+    server.setHandler(rh);
 
     server.start();
     server.join();
