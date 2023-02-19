32cdb774dc9c0ef154a73b313d9786b4f9287f21
lindxdeep <linder.igor@gmail.com>
2023-02-19 21:26:33 +0300

    InetAddress

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,25 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * App
  */
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws UnknownHostException {
 
-    System.out.println("Hello");
+    InetAddress iAddr = InetAddress.getLocalHost();
+    System.out.println(iAddr);                        // lindx/127.0.1.1
 
+    System.out.println(iAddr.getHostName());          // lindx
+    System.out.println(iAddr.getHostAddress());       // 127.0.1.1
+
+    System.out.println(iAddr.getCanonicalHostName()); // lindx
+    System.out.println(iAddr.getLoopbackAddress());   // localhost/127.0.0.1
+
+    byte[] address = iAddr.getAddress();
+
+    for (byte b : address)
+      System.out.print(b);                            // 127011
   }
 }
